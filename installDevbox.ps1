d66046a864c39fd06d598c368db25db77eebebe3
WeLytics <2650967+kellerfabian@users.noreply.github.com>
2023-01-08 15:36:59 +0100

    Update installDevbox.ps1

--- installDevbox.ps1
+++ installDevbox.ps1
@@ -21,7 +21,7 @@ choco install irfanview irfanviewplugins -y
 choco install visualstudio2022professional -y
 choco install visualstudio2022-workload-manageddesktop -y
 choco install visualstudio2022-workload-netweb -y
-
+choco install totalcommander -y
 
 #choco install Microsoft-Hyper-V-All -source windowsFeatures
 choco install IIS-WebServerRole -source windowsfeatures
