9cc0fc4c2a9d587f9da4a00e7fcfcd76d4995e69
WeLytics <2650967+kellerfabian@users.noreply.github.com>
2023-01-08 13:38:40 +0100

    Update installDevbox.ps1

--- installDevbox.ps1
+++ installDevbox.ps1
@@ -1,9 +1,16 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 Set-WinUserLanguageList -LanguageList de-CH -Force
 
-
-
 Import-Module Boxstarter.Chocolatey
 Set-ExplorerOptions -showHiddenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
 
+choco install sysinternals
 choco install teamviewer
+choco install git
+choco install fiddler4
+choco install git-credential-winstore
+choco install notepadplusplus
+choco install dotpeek
+
+#choco install Microsoft-Hyper-V-All -source windowsFeatures
+choco install IIS-WebServerRole -source windowsfeatures
